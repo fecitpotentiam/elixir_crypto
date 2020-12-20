68568d935979d07f1eca1f0bcb6c12a780d4f88a
fecitpotentiam <seeingyouinhell@gmail.com>
2020-12-20 17:50:52 +0500

    Renamed project

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Crypto.MixProject do
 
   def project do
     [
-      app: :kv,
+      app: :elixir_crypto,
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
