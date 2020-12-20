fe8af52f7d579e23d8472650fe5e25c9268259cf
fecitpotentiam <seeingyouinhell@gmail.com>
2020-12-20 16:56:14 +0500

    Added Code Word cipher

---
+++ lib/code_word.ex
@@ -0,0 +1,57 @@
+defmodule CodeWord do
+  @moduledoc """
+  Code Word cipher implementation
+  """
+  import Enum
+  @doc """
+  Get a map of english alphabet and encrypted alphabet
+  """
+  defp get_alphabet_map(code_word) do
+    alphabet = map(to_list(?a..?z), fn(n) -> <<n>> end)
+    head = String.graphemes(code_word)
+           |> uniq
+    tail =  Enum.filter(alphabet, fn el -> !Enum.member?(head, el) end)
+    zip(alphabet, head ++ tail)
+    |> into(%{})
+  end
+
+  @doc """
+  Get a reversed alphabet map (for decrypting)
+  """
+  defp prepare_alphabet(alphabet, :true) do
+    for {k, v} <- alphabet, into: %{}, do: {v, k}
+  end
+
+  @doc """
+  Get a straight alphabet map (for encrypting)
+  """
+  defp prepare_alphabet(alphabet, :false) do
+    alphabet
+  end
+
+  @doc """
+  Get encrypted or decrypted string
+  """
+  defp encrypt_string(string, code_word, reverse \\ :false) do
+    alphabet_map = prepare_alphabet(get_alphabet_map(code_word), reverse)
+    IO.inspect(alphabet_map)
+    String.graphemes(string)
+    |> map(fn g -> alphabet_map[g] end)
+    |> join("")
+  end
+
+  @doc """
+  Get encrypted string
+  """
+  def encrypt(string, code_word) do
+    encrypt_string(string, code_word)
+  end
+
+  @doc """
+  Get decrypted string
+  """
+  def decrypt(string, code_word) do
+    encrypt_string(string, code_word, :true)
+  end
+
+end
